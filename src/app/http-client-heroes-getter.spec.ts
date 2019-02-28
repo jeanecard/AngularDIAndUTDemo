55dc874b6d532ecfab1a82352a07a55190025c61
jeanecard <jean.ecard@outlook.fr>
2019-02-28 17:54:43 +0100

    Delete http-client-heroes-getter.spec.ts

--- src/app/http-client-heroes-getter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { HttpClientHeroesGetter } from './http-client-heroes-getter';
-
-describe('HttpClientHeroesGetter', () => {
-  it('should create an instance', () => {
-    expect("Here must be coded the previous heroes service glue to httpClient").toBeTruthy();
-  });
-});
